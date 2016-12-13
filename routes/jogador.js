256a5ba87377354792280d27c4521b2e6ccff8d3
mlkdd1988 <mlkdd1988@gmail.com>
2016-12-13 11:52:47 -0200

    tete

--- routes/jogador.js
+++ routes/jogador.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var mongoose = require('mongoose');
-var Jogador = require('../models/jogador.js');
+var Jogador = require('../models/Jogador');
 
 /* GET /time Listagem de usuários. */
 router.get('/', function(req, res, next) {
--- routes/time.js
+++ routes/time.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var mongoose = require('mongoose');
-var Time = require('../models/Time.js');
+var Time = require('../models/Time');
 
 /* GET /time Listagem de usuários. */
 router.get('/', function(req, res, next) {
