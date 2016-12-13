20468b1834ec6d846bb27c5f16b9c4e8f7800893
mlkdd1988 <mlkdd1988@gmail.com>
2016-12-13 12:06:12 -0200

    tete

--- routes/time.js
+++ routes/time.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var mongoose = require('mongoose');
-var Time = require('../models/Time');
+var Time = require('../models/Time.js');
 
 /* GET /time Listagem de usuários. */
 router.get('/', function(req, res, next) {
